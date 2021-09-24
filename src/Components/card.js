e94227426421a09d0b8e90d949220ab6c726fcd5
James Tan <tan0929@outlook.com>
2021-09-23 19:10:43 -0700

    lazyload

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,12 +1,14 @@
 import '../Styles/card.scoped.css'
 import Flip from  'react-card-flip'
 import {useState} from 'react'
+import LazyLoad from 'react-lazy-load'
 
 function Card({data}){
   const [flip, setFlip] = useState(false)
   return(
     <div className="wrapper">
       <div onClick={()=>setFlip(!flip)}>
+        <LazyLoad>
         <Flip isFlipped={flip}>
             <div className='front card'>
               <img src={data.front} alt="card front" />
@@ -14,7 +16,8 @@ function Card({data}){
             <div className='back card'>
               <img src={data.back} alt="card back"/>
             </div>
-        </Flip> 
+        </Flip>
+        </LazyLoad>
       </div>
       <div className='audio-wrapper'>
         <audio controls src={data.audio}>
