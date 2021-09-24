1be1d30c8915f0a8ac46dd1da7ea5540f2509222
James Tan <tan0929@outlook.com>
2021-09-24 15:18:29 -0700

    test

--- src/service-worker.js
+++ src/service-worker.js
@@ -50,9 +50,9 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && (url.pathname.endsWith('.jpg')|| url.pathname.endsWith('.mp3')), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => url.origin === self.location.origin && (url.pathname.endsWith('.jpg') || url.pathname.endsWith('.mp3')), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
-    cacheName: 'resource',
+    cacheName: 'media',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
